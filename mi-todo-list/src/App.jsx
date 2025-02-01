1b8941d7e5482b1e8e427af82e13cf8c0c403285
Dixon Sequera <dixonsequera@MacBook-Air-de-Dixon.local>
2025-02-01 17:32:15 +0100

    finalizado

--- mi-todo-list/src/App.jsx
+++ mi-todo-list/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
@@ -28,47 +26,32 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>CREADOR  DE  TAREAS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          CONTADOR {count}
-        </button>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <input
-          type="text"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          onKeyDown={handleAddTask}
-          placeholder="Añadir nueva tarea"
+    <div className="App">
+      <h1>Lista de Tareas</h1>
+      <div className="task-container">
+        <input 
+          type="text" 
+          value={newTask} 
+          onChange={(e) => setNewTask(e.target.value)} 
+          onKeyDown={handleAddTask} 
+          placeholder="Nueva tarea"
         />
         <ul>
           {tasks.map((task, index) => (
-            <li key={index} className="button-one" style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+            <li key={index} className={`task-item ${task.completed ? "completed" : ""}`}>
+              <input 
+                type="checkbox" 
+                checked={task.completed} 
+                onChange={() => handleToggleComplete(index)} 
+              />
               {task.text}
-              <button onClick={() => handleToggleComplete(index)}>
-                {task.completed ? 'Desmarcar' : 'Completar'}
-              </button>
               <button onClick={() => handleDeleteTask(index)}>Eliminar</button>
             </li>
           ))}
         </ul>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
